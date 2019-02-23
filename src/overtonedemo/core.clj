dc5063d6af4dac1eff1ea609c19032d654c2aaef
ParikshithMohan <parikshithmohan@gmail.com>
2019-02-23 12:19:57 +0530

    second commit

--- src/overtonedemo/core.clj
+++ src/overtonedemo/core.clj
@@ -5,19 +5,4 @@
   [x]
   (println x "Hello, World!"))
 
-(definst trem [freq 2500 depth 30 rate 5 length 7]
-  (* 0.3
-     (line:kr 0 1 length FREE)
-     (saw (+ freq (* depth (sin-osc:kr rate))))))
-(def f (freesound 205789))
-(f)
-(def f-loop(f :rate 0.5 :loop? 1))
-
-(kill)
-(stop)
-
-
-(trem) q
-()
-
 
